e0644a95bea9bd60132acdf23d4d46757cfb7622
Armando Sandino <jonwinlive@gmail.com>
2020-04-20 00:25:46 -0600

    una dedo angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
+import { PieComponent } from './components/pie/pie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    PieComponent
   ],
   imports: [
     BrowserModule
